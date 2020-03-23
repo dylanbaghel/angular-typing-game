49f7b15130c1c47919b79cdcc823ecfa7be4e8a9
Dylan Baghel <baghelabhi39@gmail.com>
2020-03-23 23:11:08 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'typing-app';
+  randomText = lorem.paragraph(3);
+  enteredText = '';
+  wrongLetters = [];
+
+
+  handleInputChange(value: string) {
+    this.enteredText = value;
+    this.wrongLetters = [];
+    this.randomText.split("").forEach((char, index) => {
+      if (char !== value[index]) {
+        this.wrongLetters.push({ correct: char, typed: value[index] });
+      }
+    });
+  }
+
+  getClass(randomLetter: string, enteredLetter: string) {
+    if (typeof enteredLetter === 'undefined') {
+      return 'unset';
+    }
+
+    if (randomLetter === " " && randomLetter !== enteredLetter) {
+      return 'danger-bg';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'wrong';
+  }
+
+  calculateAccuracy() {
+    return ((this.randomText.length - this.wrongLetters.length) / this.randomText.length) * 100;
+  }
+
+  reload() {
+    location.reload();
+  }
 }
